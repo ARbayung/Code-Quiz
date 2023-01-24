57d2e4bba49ed8a392f63cc2552f3ce4f2ac810b
Abiskar Rai <a.bayung28@gmail.com>
2023-01-24 17:56:38 +0000

    application now goes to end screen when timer finishes

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,16 @@ var timerElement = document.querySelector("#time");
 var start = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
 var QScreen = document.querySelector("#questions");
-var question = document.querySelector("#question-title");
+var questionTitle = document.querySelector("#question-title");
 var choices = document.querySelector("#choices");
 var score = document.querySelector("#final-score");
+var endScreen = document.querySelector("#end-screen");
+
 
 var timer;
 var timerCount;
 var scoreCounter;
+var gameOver = false;
 
 function startQuiz(){
     //setting start time to 10 seconds
@@ -23,10 +26,11 @@ function startQuiz(){
     //initialising timer function
     startTimer();
     question1();
+    question2
 }
 
 function question1(){
-    question.textContent = "what is my fav color?";
+    questionTitle.textContent = "what is my fav color?";
 
     var listUL = document.createElement("ul");
     var li1 = document.createElement("li");
@@ -40,22 +44,58 @@ function question1(){
     li3.textContent = "red";
     li4.textContent = "yellow";
 
+    li1.classList.add("correct");
+    li2.classList.add("incorrect");
+    li3.classList.add("incorrect");
+    li4.classList.add("incorrect");
+
+
     choices.appendChild(listUL);
     listUL.appendChild(li1);
     listUL.appendChild(li2);
     listUL.appendChild(li3);
     listUL.appendChild(li4);
 
+    checkQuestion();
+
+}
+
+function question2(){
+    questionTitle.textContent = "who am i?";
+
+    li1.textContent = "abs";
+    li2.textContent = "jess";
+    li3.textContent = "darem";
+    li4.textContent = "anmol";
+
+    li1.classList.add("correct");
+    li2.classList.add("incorrect");
+    li3.classList.add("incorrect");
+    li4.classList.add("incorrect");
+
+
+    choices.appendChild(listUL);
+    listUL.appendChild(li1);
+    listUL.appendChild(li2);
+    listUL.appendChild(li3);
+    listUL.appendChild(li4);
+
+    checkQuestion();
+
+}
+
+function checkQuestion(){
     choices.addEventListener("click", function(event){
         
         var element = event.target;
         var correct = document.querySelector(".correct");
 
-        if(element = li1){
+        if(element.matches(".correct")){
             timerCount = timerCount + 5;
+        }else if(element.matches(".incorrect")){
+            timerCount = timerCount - 5;
         }
     })
-
 }
 
 function startTimer(){
@@ -69,9 +109,13 @@ function startTimer(){
         //stops timer from going below 0, clears timer
         if(timerCount === 0) {
             clearInterval(timer);
+            QScreen.classList.add("hide");
+            endScreen.classList.remove("hide");
         }
     //controls speed of timer 1000 being one second
     }, 1000);
 }
+
+
 
 start.addEventListener("click", startQuiz);
